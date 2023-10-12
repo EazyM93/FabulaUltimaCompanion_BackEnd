595e3f064675834dd52080f4ca69ceb6765833b6
EazyM93 <centini.manuel.93@gmail.com>
2023-10-13 00:14:34 +0200

    Spring Project Creation

---
+++ FabulaUltimaCompanion_BackEnd/src/main/java/eazym/FabulaUltimaCompanion_BackEnd/FabulaUltimaCompanionBackEndApplication.java
@@ -0,0 +1,13 @@
+package eazym.FabulaUltimaCompanion_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabulaUltimaCompanionBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabulaUltimaCompanionBackEndApplication.class, args);
+	}
+
+}
---
+++ FabulaUltimaCompanion_BackEnd/src/test/java/eazym/FabulaUltimaCompanion_BackEnd/FabulaUltimaCompanionBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package eazym.FabulaUltimaCompanion_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabulaUltimaCompanionBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
