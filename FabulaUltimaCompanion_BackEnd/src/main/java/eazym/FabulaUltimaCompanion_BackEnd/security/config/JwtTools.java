1314358210084f520d8ac3b4c59582c11a2af579
EazyM93 <centini.manuel.93@gmail.com>
2023-10-13 00:51:54 +0200

    Minor fix

--- FabulaUltimaCompanion_BackEnd/src/main/java/eazym/FabulaUltimaCompanion_BackEnd/security/config/JwtTools.java
+++ FabulaUltimaCompanion_BackEnd/src/main/java/eazym/FabulaUltimaCompanion_BackEnd/security/config/JwtTools.java
@@ -13,7 +13,7 @@ import java.util.Date;
 public class JwtTools {
 
     @Value("${spring.jwt.secret}")
-    private String secretKey;
+    String secretKey;
 
     public String createToken(User user) {
 
